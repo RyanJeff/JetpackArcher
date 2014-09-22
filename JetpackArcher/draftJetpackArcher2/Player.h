368201be9d1e88169e4c9792852117480d6deaab
nicolesilverthorn <nicolesilverthorn@hotmail.com>
2014-09-22 13:08:14 -0400

    new proj with player class

---
+++ JetpackArcher/draftJetpackArcher2/Player.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Sprite.h"
+
+class Player : public Sprite
+{
+private:
+	const float JUMP_FORCE;
+
+public:
+	Player(void) : JUMP_FORCE(10.0f)
+	{}
+
+	Player(FXMVECTOR pos2D, FXMVECTOR scale2D, uint16_t frameWidth, uint16_t frameHeight, float depth, const std::vector<Frame*>& frames,
+		float frameRate, ID3D11Device* device) :
+			Sprite(pos2D, scale2D, frameWidth, frameHeight, depth, frames, frameRate, device),
+			JUMP_FORCE(10.0f)
+	{
+	}
+
+	~Player(void);
+
+	void Jump();
+};
