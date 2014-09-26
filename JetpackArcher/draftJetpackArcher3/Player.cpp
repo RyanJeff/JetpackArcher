86659d503840b8f54e63a35d037cc90b1774e1f2
phbickle <phbickle@gmail.com>
2014-09-26 04:01:45 -0400

    Collision with ceiling temp fix
    Added a check so the player no longer goes through the ceiling while
    using the jetpack.

--- JetpackArcher/draftJetpackArcher3/Player.cpp
+++ JetpackArcher/draftJetpackArcher3/Player.cpp
@@ -18,7 +18,6 @@ void Player::Jump()
 
 void Player::UseJetpack(float dt)
 {
-
 	//Check to see if jet pack is recharged and resets the useage timer.
 	if (jetPackRecharge == 0.0f)
 	{
@@ -32,8 +31,12 @@ void Player::UseJetpack(float dt)
 		{
 			jetPackUseage += dt;
 			XMVECTOR vel = XMLoadFloat3(&mVelocity);
-			vel = XMVectorSet(vel.m128_f32[0], vel.m128_f32[1] + (JUMP_FORCE / 150), vel.m128_f32[2], 0.0f);
-			XMStoreFloat3(&mVelocity, vel);
+			if (vel.m128_f32[1] < JUMP_FORCE)
+			{
+				vel = XMVectorSet(vel.m128_f32[0], vel.m128_f32[1] + (JUMP_FORCE / 150.0f), vel.m128_f32[2], 0.0f);
+				XMStoreFloat3(&mVelocity, vel);
+			}
+			
 		}
 
 	}
